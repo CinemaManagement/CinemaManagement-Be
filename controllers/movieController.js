c9b8a025f6c6d5411836b6f6435897275d998ed4
Lilyhouser <houselily010@gmail.com>
2026-03-23 00:26:03 +0700

    add cache for active movie

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,10 +1,27 @@
 const Movie = require("../models/Movie");
 const { ROLE } = require("../constraints/role");
 const STATUS = require("../constraints/status");
+const redisClient = require("../config/redis");
+
+const updateCacheMovie = async (newMovies) => {
+  try {
+    await redisClient.set("active-movies", JSON.stringify(newMovies), {
+      EX: 60 * 60 * 24,
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Error when update cache movie");
+  }
+};
 
 const getAllActiveMovies = async (req, res) => {
   try {
+    const cacheMovie = await redisClient.get("active-movies");
+    if (cacheMovie) {
+      return res.status(200).json(JSON.parse(cacheMovie));
+    }
     const movies = await Movie.find({ status: STATUS.ACTIVE });
+    await updateCacheMovie(movies);
     res.status(200).json(movies);
   } catch (error) {
     console.error(error);
@@ -40,6 +57,8 @@ const addMovie = async (req, res) => {
       revenueSharePercent,
       status: STATUS.ACTIVE,
     });
+    const movies = await Movie.find({ status: STATUS.ACTIVE });
+    await updateCacheMovie(movies);
     res.status(201).json(movie);
   } catch (error) {
     console.error(error);
@@ -57,6 +76,8 @@ const updateMovie = async (req, res) => {
         .status(404)
         .json({ message: `Not found movie with id ${id}!` });
     }
+    const movies = await Movie.find({ status: STATUS.ACTIVE });
+    await updateCacheMovie(movies);
     res.status(200).json(movie);
   } catch (error) {
     console.error(error);
@@ -77,6 +98,8 @@ const hideMovie = async (req, res) => {
         .status(404)
         .json({ message: `Not found movie with id ${id}!` });
     }
+    const movies = await Movie.find({ status: STATUS.ACTIVE });
+    await updateCacheMovie(movies);
     res
       .status(200)
       .json({ message: `Movie ${movie.title} hidden successfully!` });
